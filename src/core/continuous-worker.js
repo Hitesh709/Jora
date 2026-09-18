08e52ae2ab8e44475a6fec477d94d27e9ac8bb31
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-18 23:02:32 +0530

    Harden champion persistence and continuous evolution

--- src/core/continuous-worker.js
+++ src/core/continuous-worker.js
@@ -5,13 +5,13 @@ export class ContinuousWorker {
     this.running=false; this.cycles=0;
   }
 
-  async run() {
+  async run({command,context={}}={}) {
     if (this.running) throw new Error("worker already running");
     this.running=true; this.cycles=0;
     try {
       while (this.running && this.cycles < this.maxCycles) {
         this.cycles += 1;
-        await this.cycle({cycle:this.cycles});
+        await this.cycle({cycle:this.cycles,command,context});
         if (!this.running || this.cycles >= this.maxCycles) break;
         await new Promise(resolve=>setTimeout(resolve,this.intervalMs));
       }
