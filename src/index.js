e3bfec813abe33e9268a7ded75753416fb118e06
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-18 22:12:33 +0530

    feat: build Jora evolution OS foundation

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { TaskRegistry, STATUS } from "./core/task-registry.js";
+export { Orchestrator } from "./core/orchestrator.js";
+export { Evaluator } from "./core/evaluator.js";
+export { EvolutionEngine } from "./core/evolution.js";
