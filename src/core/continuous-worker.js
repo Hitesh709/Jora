63b07ee60e1f4aaa4d295b812b3e0fdd09b39201
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-18 22:29:33 +0530

    Add src/core/continuous-worker.js

---
+++ src/core/continuous-worker.js
@@ -0,0 +1,23 @@
+export class ContinuousWorker {
+  constructor({cycle, intervalMs=60_000, maxCycles=Infinity}={}) {
+    if (!cycle) throw new Error("cycle is required");
+    this.cycle=cycle; this.intervalMs=intervalMs; this.maxCycles=maxCycles;
+    this.running=false; this.cycles=0;
+  }
+
+  async run() {
+    if (this.running) throw new Error("worker already running");
+    this.running=true; this.cycles=0;
+    try {
+      while (this.running && this.cycles < this.maxCycles) {
+        this.cycles += 1;
+        await this.cycle({cycle:this.cycles});
+        if (!this.running || this.cycles >= this.maxCycles) break;
+        await new Promise(resolve=>setTimeout(resolve,this.intervalMs));
+      }
+    } finally { this.running=false; }
+    return {cycles:this.cycles};
+  }
+
+  stop() { this.running=false; }
+}
