0c438e54909e690447f6a9f20a14a42bee72c4dc
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-18 22:56:05 +0530

    Export production runtime components

--- src/index.js
+++ src/index.js
@@ -45,3 +45,6 @@ export { createJoraRuntime } from "./core/runtime-composer.js";
 export { ModelProjectBuilder } from "./core/model-project-builder.js";
 export { createProjectTestRunner } from "./core/project-test-runner.js";
 export { AutonomousBuildPipeline } from "./core/autonomous-build-pipeline.js";
+export { WorkspaceRepository } from "./core/workspace-repository.js";
+export { createWorkspaceSecurityCouncil } from "./core/security-checks.js";
+export { createProductionJoraRuntime } from "./core/production-runtime.js";
