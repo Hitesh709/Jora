5fcf09c2094f5bc0b59f9887cf1ced01afe5ce9a
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-20 19:47:14 +0530

    test: validate generated test failures from runner output

--- test/jora-native-coding-acceptance.test.js
+++ test/jora-native-coding-acceptance.test.js
@@ -23,7 +23,7 @@ async function runGeneratedTests(root){
     let stdout="",stderr="";
     child.stdout.on("data",x=>stdout+=x);
     child.stderr.on("data",x=>stderr+=x);
-    child.on("close",code=>resolve({ok:code===0,code,stdout,stderr}));
+    child.on("close",code=>{\n      const output=stdout+"\\n"+stderr;\n      const failedTests=/# fail\\s+[1-9]/.test(output);\n      resolve({ok:code===0&&!failedTests,code,stdout,stderr});\n    });
   });
 }
 
