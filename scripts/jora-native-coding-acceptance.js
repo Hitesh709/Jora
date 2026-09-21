8d4bffc6e147081e8374db25d1b8b7aa5ab9397a
Hitesh709 <Hiteshkansara4@gmail.com>
2026-09-21 22:43:37 +0530

    test: include Universal Builder acceptance coverage

--- scripts/jora-native-coding-acceptance.js
+++ scripts/jora-native-coding-acceptance.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import {spawn} from "node:child_process";
-const child=spawn(process.execPath,["--test","test/jora-native-coding-acceptance.test.js"],{stdio:"inherit"});
+const child=spawn(process.execPath,["--test","test/jora-native-coding-acceptance.test.js","test/universal-project-generator.test.js"],{stdio:"inherit"});
 child.on("exit",code=>process.exit(code??1));
